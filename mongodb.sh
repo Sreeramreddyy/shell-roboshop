42888fe688cc8df0fec12f443029a23489223d02
Sreeram Reddy <sreeramlearning25@gmail.com>
2025-11-29 04:28:46 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -39,3 +39,9 @@ VALIDATE $? "Enable MongoDB"
 
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Start mongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
